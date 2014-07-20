03367e89e5c68b88253a3a2aa141235c5cec838c
Dave Syer <dsyer@gopivotal.com>
2014-07-20 17:42:03 +0100

    Add manifest

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -1,17 +1,9 @@
 package demo;
 
-import java.util.Arrays;
-
-import org.cloudfoundry.community.servicebroker.model.Catalog;
-import org.cloudfoundry.community.servicebroker.model.ServiceDefinition;
-import org.cloudfoundry.community.servicebroker.service.BeanCatalogService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.platform.cloudfoundry.broker.FreeServiceDefinitionFactory;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -21,23 +13,6 @@ public class Application extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Bean
-	public BeanCatalogService catalogService() {
-		return new BeanCatalogService(catalog());
-	}
-
-	@Bean
-	public Catalog catalog() {
-		return new Catalog(Arrays.asList(serviceDefinition()));
-	}
-
-	@Bean
-	@ConfigurationProperties("service.definition")
-	public ServiceDefinition serviceDefinition() {
-		return new FreeServiceDefinitionFactory("config-").create("configserver",
-				"Spring platform config server");
 	}
 
 }
