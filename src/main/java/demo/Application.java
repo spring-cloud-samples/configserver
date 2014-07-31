ad214370b1738a8371b4dc51b0a0ac6fb5479a67
Dave Syer <dsyer@gopivotal.com>
2014-07-31 16:08:49 -0700

    Use @EnableEurekaClient

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.platform.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableAutoConfiguration
 @Import(org.springframework.platform.config.server.Application.class)
+@EnableEurekaClient
 public class Application extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
