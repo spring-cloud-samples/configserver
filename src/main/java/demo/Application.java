01f9ff02752270992727493bc4adaea12ead742c
Dave Syer <dsyer@gopivotal.com>
2014-08-01 15:02:35 -0700

    Switch to @Enable everything

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -3,15 +3,14 @@ package demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.platform.config.server.EnableConfigServer;
 import org.springframework.platform.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableAutoConfiguration
-@Import(org.springframework.platform.config.server.Application.class)
 @EnableEurekaClient
-public class Application extends WebMvcConfigurerAdapter {
+@EnableConfigServer
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
