dea30a3368d515cc66224d8d2c116a2823ca10a4
Dave Syer <dsyer@pivotal.io>
2014-08-10 08:44:59 -0700

    Make app class names unique

--- src/main/java/demo/ConfigServerApplication.java
+++ src/main/java/demo/ConfigServerApplication.java
@@ -10,10 +10,10 @@ import org.springframework.platform.netflix.eureka.EnableEurekaClient;
 @EnableAutoConfiguration
 @EnableEurekaClient
 @EnableConfigServer
-public class Application {
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
 }
