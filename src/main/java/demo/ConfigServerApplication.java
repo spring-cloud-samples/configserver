97809af3b6cb45632a902bb93f75fac626ce0c53
Dave Syer <dsyer@pivotal.io>
2014-08-29 13:34:36 +0100

    Update package names

--- src/main/java/demo/ConfigServerApplication.java
+++ src/main/java/demo/ConfigServerApplication.java
@@ -3,8 +3,8 @@ package demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.platform.config.server.EnableConfigServer;
-import org.springframework.platform.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @Configuration
 @EnableAutoConfiguration
