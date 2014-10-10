537e4983ef12d49589a255f81c102e3391dcfc78
Dave Syer <dsyer@pivotal.io>
2014-10-10 15:57:51 +0100

    Add HTTP Basic security

--- src/main/java/demo/ConfigServerApplication.java
+++ src/main/java/demo/ConfigServerApplication.java
@@ -2,9 +2,9 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
