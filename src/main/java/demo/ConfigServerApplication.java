5004b20d897696f00e836b308ddee39013af74fd
Dave Syer <dsyer@pivotal.io>
2015-02-25 17:12:50 +0000

    Config package name change

--- src/main/java/demo/ConfigServerApplication.java
+++ src/main/java/demo/ConfigServerApplication.java
@@ -3,7 +3,7 @@ package demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.configure.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
